6431a01e4ebfd719fc02acf31a064834d4ac635c
diyaa <diyaaine@gmail.com>
2021-04-19 08:35:48 -0400

    final

--- Chats/Chats.py
+++ Chats/Chats.py
@@ -20,7 +20,6 @@ class Log:
         values = {"$set": cases_dict}
         records = dbConn.cases_records
         records.update_one(query, values)
-        # For 1 st case.. then we will keep updating
         # records.insert_one(cases_dict)
 
     def get_cases(self, search, botmessage, dbConn):
